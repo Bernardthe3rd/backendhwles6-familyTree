b2fbbb5c3cd0c7d21e549660ca3e2cbba2531394
Bernardthe3rd <bammeijer@outlook.com>
2024-06-19 16:58:37 +0200

    made tests for some getters and setters for the Person class

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -81,4 +82,156 @@ class PersonTest {
         //assert
         assertEquals(grandChildren.size(), 2);
     }
+
+    //Below are the tests for all the getters and setters from the Person class.
+    @Test
+    void shouldGetName() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.getName();
+        //assert
+        assertEquals("Getty", person.getName());
+    }
+
+    @Test
+    void shouldSetName() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.setName("Henriette");
+        //assert
+        assertEquals("Henriette", person.getName());
+    }
+
+    @Test
+    void shouldGetMiddleName() {
+        //arrange
+        Person person = new Person("Getty", "Petty", "Thierry", "Female", 21);
+        //act
+        person.getMiddleName();
+        //assert
+        assertEquals("Petty", person.getMiddleName());
+    }
+
+    @Test
+    void shouldSetMiddleName() {
+        //arrange
+        Person person = new Person("Getty", "Petty", "Thierry", "Female", 21);
+        //act
+        person.setMiddleName("Verpetty");
+        //assert
+        assertEquals("Verpetty", person.getMiddleName());
+    }
+
+    @Test
+    void shouldGetLastName() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.getLastName();
+        //assert
+        assertEquals("Thierry", person.getLastName());
+    }
+
+    @Test
+    void shouldSetLastName() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.setLastName("Fury");
+        //assert
+        assertEquals("Fury", person.getLastName());
+    }
+
+    @Test
+    void shouldGetSex() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.getSex();
+        //assert
+        assertEquals("Female", person.getSex());
+    }
+
+    @Test
+    void shouldSetSex() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.setSex("Male");
+        //assert
+        assertEquals("Male", person.getSex());
+    }
+
+    @Test
+    void shouldGetAge() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.getAge();
+        //assert
+        assertEquals(21, person.getAge());
+    }
+
+    @Test
+    void shouldSetAge() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.setAge(44);
+        //assert
+        assertEquals(44, person.getAge());
+    }
+
+    @Test
+    void shouldGetMother() {
+        //arrange
+        Person person = new Person("Getty", "Thierry", "Female", 21);
+        //act
+        person.getAge();
+        //assert
+        assertEquals(21, person.getAge());
+    }
+
+    @Test
+    void shouldSetMother() {
+        //arrange
+        Person mother = new Person("Getty", "Thierry", "Female", 29);
+        Person child = new Person("Jordy", "Defoe", "Male", 2);
+        //act
+        child.setMother(mother);
+        //assert
+        assertEquals(child.getMother(), mother);
+    }
+
+    @Test
+    void shouldSetFather() {
+        //arrange
+        Person father = new Person("Willem", "Defoe", "Male", 40);
+        Person child = new Person("Jordy", "Defoe", "Female", 12);
+        //act
+        child.setFather(father);
+        //assert
+        assertEquals(child.getFather(), father);
+    }
+
+    @Test
+    void shouldSetSiblings() {
+        //arrange
+        Person sibling1 = new Person("Kwik", "Duck", "Male", 14);
+        Person sibling2 = new Person("Kwak", "Duck", "Female", 12);
+        //act
+        sibling1.addSiblings(sibling2);
+        sibling2.addSiblings(sibling1);
+
+        sibling1.setSiblings(sibling2.getSiblings());
+        sibling2.setSiblings(sibling1.getSiblings());
+
+        //assert
+        assertEquals(sibling1.getSiblings().size(), 1);
+        assertEquals(sibling2.getSiblings().size(), 1);
+    }
+
+    
 }
